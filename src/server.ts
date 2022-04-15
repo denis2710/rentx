78e0e4d6799e738a0c9967f961d79aadf8edf58e
denis2710 <denispereiraalves@gmail.com>
2022-04-15 13:20:30 -0300

    feat: :technologist: Add Error handling

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors'
 import "reflect-metadata"
 import { router } from './routes';
 import swaggerUi from 'swagger-ui-express'
@@ -7,6 +8,7 @@ import YAML from 'yamljs';
 import './database'
 
 import './shared/container'
+import { AppError } from './erros/AppError';
 
 const app = express();
 
@@ -25,6 +27,14 @@ app.get('/',( request, response) => {
 app.post('/courses', (req, res) => {
     const {name} = req.body;
     return res.json({name: name})
+})
+
+app.use((err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    if(err instanceof AppError) {
+        return response.status(err.statusCode).json({message: err.message})
+    }
+
+    return response.status(500).json({message: `Internal Server Error - ${err.message}`})
 })
 
 app.listen(3333, ()=> console.log('Server running'))

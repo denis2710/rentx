5995e7c2667aafb68689c99d8d82d8568180deda
denis2710 <denispereiraalves@gmail.com>
2022-04-18 14:39:28 -0300

    feat: :clown_face: Add Category Repository mock

---
+++ src/modules/cars/repositories/in-memory/CategoryRepositoryInMemory.ts
@@ -0,0 +1,27 @@
+import { Category } from "../../entites/Category";
+import { ICategoryRepository, ICreateCategoryDTO } from "../ICategoryRepository";
+
+class CategoryRepositoryInMemory implements ICategoryRepository {
+
+    categories: Category[] = [];
+
+    async findByName(name: string): Promise<Category> {
+        const category = this.categories.find(c => c.name === name);
+        return category;
+    }
+    async list(): Promise<Category[]> {
+        const all = this.categories
+        return all
+    }
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+      const category = new Category()
+      Object.assign(category,{
+          name, 
+          description
+      })  
+      
+      this.categories.push(category);
+    } 
+}
+
+export { CategoryRepositoryInMemory }

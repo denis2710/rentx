3f4e62d106a0b5658c1f392cd3d645b25928af5c
denis2710 <denispereiraalves@gmail.com>
2022-04-27 21:00:22 -0300

    test: :white_check_mark: Add tests to create categories

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,44 @@
+import { AppError } from "../../../../erros/AppError";
+import { CategoryRepositoryInMemory } from "../../repositories/in-memory/CategoryRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
+
+let createCategoryUseCase: CreateCategoryUseCase
+let categoryRepositoryInMemory: CategoryRepositoryInMemory;
+
+describe('Create Category', () => {
+
+    beforeEach(() => {
+        categoryRepositoryInMemory = new CategoryRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(categoryRepositoryInMemory)
+    })
+    
+    it('should be able to create a new Category', async () => {
+        const category = {
+            name: 'category name test',
+            description: 'category description test'
+        }
+
+        await createCategoryUseCase.execute(category)
+
+        const categoryCreated = await categoryRepositoryInMemory.findByName(category.name)
+
+        expect(categoryCreated).toHaveProperty("id")
+
+    })
+
+    it('should not be able to create a new Category with name exists', async () => {
+    
+        expect(async () => {
+            const category = {
+                name: 'category name test',
+                description: 'category description test'
+            }
+    
+            await createCategoryUseCase.execute(category)
+            await createCategoryUseCase.execute(category)
+
+        }).rejects.toBeInstanceOf(AppError)
+     
+
+    })
+})
